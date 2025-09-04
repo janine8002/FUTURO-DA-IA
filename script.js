0e0429f4a5423a338776440299e1c118654dd3af
jaine <145770837+janine8002@users.noreply.github.com>
2025-09-04 11:39:29 -0300

    kk

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const caixaPrincipal = document.querySelector(".caixa-principal);
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const caixaResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "O que é a Inteligência Artificial?",
+        alternativas: [
+            "A) Tecnologia que simula processos cognitivos humanos, como aprendizado e resolução de problemas",
+"B) Programação de computadores para realizar tarefas simples, como cálculos matemáticos",
+        ]
+    },
+    {
+        enunciado: "Qual das seguintes áreas NÃO é considerada parte da IA?",
+        alternativas: [
+          "A) Aprendizado de Máquina",
+"B) Processamento de Linguagem Natural",  
+        ]
+
+    }
