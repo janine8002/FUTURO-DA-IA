6139ae1014113fabbabd5e66c0240b7c9ca1316a
jaine <145770837+janine8002@users.noreply.github.com>
2025-09-11 11:38:30 -0300

    hh

--- script.js
+++ script.js
@@ -39,3 +39,21 @@ const perguntas = [
            "B) Não, porque a IA não tem valores morais ou consciência.
         ]
     }
+
+    let atual = 0;
+    let perguntaAtual;
+
+    function mostraPergunta() {
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+        mostraAlternativas();
+    }
+
+    function mostraAlternativa() {
+        for (const alternativa of perguntaAtual.alternativas) {
+            const botaoAlternativas = document.createElement("button");
+            botaoAlternativas.textContent = alternativa;
+        }
+    }
+
+    mostraPergunta();
