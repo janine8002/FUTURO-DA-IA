9835db8901c441bce46d78f323644936b9ff3e88
jaine <145770837+janine8002@users.noreply.github.com>
2025-09-11 11:54:31 -0300

    jj

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const caixaPrincipal = document.querySelector(".caixa-principal);
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const caixaResultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
@@ -42,10 +43,16 @@ const perguntas = [
 
     let atual = 0;
     let perguntaAtual;
+    let historiaFinal = "";
 
     function mostraPergunta() {
+        if (atual >= perguntas.length) {
+            mostraResultado();
+            return;
+        }
         perguntaAtual = perguntas[atual];
         caixaPerguntas.textContent = perguntaAtual.enunciado;
+        caixaAlternativas.textContent = "";
         mostraAlternativas();
     }
 
