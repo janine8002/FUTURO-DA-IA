917a772550f4884454272ae80c7da533b7dc57f1
jaine <145770837+janine8002@users.noreply.github.com>
2025-09-11 11:08:53 -0300

    kmk

--- script.js
+++ script.js
@@ -5,23 +5,37 @@ const caixaResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "O que é a Inteligência Artificial?",
+        enunciado: "1) O que é a Inteligência Artificial?",
         alternativas: [
             "A) Tecnologia que simula processos cognitivos humanos, como aprendizado e resolução de problemas",
 "B) Programação de computadores para realizar tarefas simples, como cálculos matemáticos",
         ]
     },
     {
-        enunciado: "Qual das seguintes áreas NÃO é considerada parte da IA?",
+        enunciado: "2) Qual das seguintes áreas NÃO é considerada parte da IA?",
         alternativas: [
           "A) Aprendizado de Máquina",
 "B) Processamento de Linguagem Natural",  
         ]
-
     },
     {
-        enunciado: "A IA pode substituir o trabalho humano?",
+        enunciado: "3) A IA pode substituir o trabalho humano?",
         alternativas: [
             "A) Sim, em todas as áreas de trabalho",
             "B) Não, ela pode substituir apenas tarefas repetitivas ou baseadas em padrões",
-    
+        ]
+    }
+    {
+        enunciado: "4) O que é Aprendizado de Máquina?",
+        alternativas: [
+            "A) Quando um computador segue regras programadas por um humano para executar tarefas.",
+            "B) Quando um computador aprende a realizar tarefas a partir de dados e experiência, sem necessidade de programação explícita.",
+        ]
+    }
+    {
+        enunciado: "5) A IA pode tomar decisões morais?", 
+        alternativas: [
+           "A) Sim, ela pode ser programada para entender e aplicar conceitos éticos.",
+           "B) Não, porque a IA não tem valores morais ou consciência.
+        ]
+    }
