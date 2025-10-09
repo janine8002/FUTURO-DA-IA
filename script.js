d1d0f6d1b63859849298c1b561badc90405fecb4
jaine <145770837+janine8002@users.noreply.github.com>
2025-10-09 11:47:12 -0300

    j

--- script.js
+++ script.js
@@ -9,37 +9,38 @@ const perguntas = [
         enunciado: "1) O que é a Inteligência Artificial?",
         alternativas: [
             "A) Tecnologia que simula processos cognitivos humanos, como aprendizado e resolução de problemas",
-"B) Programação de computadores para realizar tarefas simples, como cálculos matemáticos",
+"B) Programação de computadores para realizar tarefas simples, como cálculos matemáticos"
         ]
     },
     {
         enunciado: "2) Qual das seguintes áreas NÃO é considerada parte da IA?",
         alternativas: [
           "A) Aprendizado de Máquina",
-"B) Processamento de Linguagem Natural",  
+"B) Processamento de Linguagem Natural"  
         ]
     },
     {
         enunciado: "3) A IA pode substituir o trabalho humano?",
         alternativas: [
             "A) Sim, em todas as áreas de trabalho",
-            "B) Não, ela pode substituir apenas tarefas repetitivas ou baseadas em padrões",
+            "B) Não, ela pode substituir apenas tarefas repetitivas ou baseadas em padrões"
         ]
-    }
+    },
     {
         enunciado: "4) O que é Aprendizado de Máquina?",
         alternativas: [
             "A) Quando um computador segue regras programadas por um humano para executar tarefas.",
-            "B) Quando um computador aprende a realizar tarefas a partir de dados e experiência, sem necessidade de programação explícita.",
+            "B) Quando um computador aprende a realizar tarefas a partir de dados e experiência, sem necessidade de programação explícita."
         ]
-    }
+    },
     {
         enunciado: "5) A IA pode tomar decisões morais?", 
         alternativas: [
            "A) Sim, ela pode ser programada para entender e aplicar conceitos éticos.",
-           "B) Não, porque a IA não tem valores morais ou consciência.
+           "B) Não, porque a IA não tem valores morais ou consciência
         ]
-    }
+    },
+];
 
     let atual = 0;
     let perguntaAtual;
@@ -67,7 +68,7 @@ const perguntas = [
 
     function respostaSelecionada(opcaoSelecionada) {
         const afirmacoes = opcaoSelecionada.afirmacao;
-        historiaFinal += afirmacoes + "";
+        historiaFinal += afirmacoes + " ";
         atual++;
         mostraPergunta();
     }
